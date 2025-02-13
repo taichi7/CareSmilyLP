1d99a542f5d4b1aeb35281ee82f00f055870f15b
info3251 <38826349-info3251@users.noreply.replit.com>
2025-02-13 10:46:28 +0000

    Agent query: ホームページが正しく表示され、特にお悩みセクションの表示が崩れていないことを確認できますか？
    Remove React Query and convert application to a static site.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/cc4e285b-489b-40c4-9444-05609d31e93b/b3440aa8-fa40-4a75-9ff0-8e523916700c.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
@@ -16,10 +14,10 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <Router />
       <Toaster />
-    </QueryClientProvider>
+    </>
   );
 }
 
