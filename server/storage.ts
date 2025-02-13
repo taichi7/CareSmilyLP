13e7d4ec283bd9d1c334801615e3ee6e351148b0
info3251 <38826349-info3251@users.noreply.replit.com>
2025-02-13 13:38:10 +0000

    Restored to 'e726bf1ad056bddd21853fc12b9a5d766929c6ec'
    Replit-Restored-To:e726bf1ad056bddd21853fc12b9a5d766929c6ec

--- server/storage.ts
+++ server/storage.ts
@@ -1,6 +1,7 @@
 import { users, type User, type InsertUser } from "@shared/schema";
-import { db } from "./db";
-import { eq } from "drizzle-orm";
+
+// modify the interface with any CRUD methods
+// you might need
 
 export interface IStorage {
   getUser(id: number): Promise<User | undefined>;
@@ -8,24 +9,31 @@ export interface IStorage {
   createUser(user: InsertUser): Promise<User>;
 }
 
-export class DatabaseStorage implements IStorage {
+export class MemStorage implements IStorage {
+  private users: Map<number, User>;
+  currentId: number;
+
+  constructor() {
+    this.users = new Map();
+    this.currentId = 1;
+  }
+
   async getUser(id: number): Promise<User | undefined> {
-    const [user] = await db.select().from(users).where(eq(users.id, id));
-    return user || undefined;
+    return this.users.get(id);
   }
 
   async getUserByUsername(username: string): Promise<User | undefined> {
-    const [user] = await db.select().from(users).where(eq(users.username, username));
-    return user || undefined;
+    return Array.from(this.users.values()).find(
+      (user) => user.username === username,
+    );
   }
 
   async createUser(insertUser: InsertUser): Promise<User> {
-    const [user] = await db
-      .insert(users)
-      .values(insertUser)
-      .returning();
+    const id = this.currentId++;
+    const user: User = { ...insertUser, id };
+    this.users.set(id, user);
     return user;
   }
 }
 
-export const storage = new DatabaseStorage();
+export const storage = new MemStorage();
