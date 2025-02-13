73c906c78a0e1ed04bdff4e61e62e8a37d190c25
nico-webapplication <nico.webapplication@gmail.com>
2025-02-14 03:12:26 +0900

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import  Header from "@/components/layout/Header";
-import  Footer  from "@/components/layout/Footer";
-import  Hero  from "@/components/sections/Hero";
-import Concerns from "@/components/sections/Concerns";
-import ProductInfo from "@/components/sections/ProductInfo";
-import  Features  from "@/components/sections/Features";
-import Benefits from "@/components/sections/Benefits";
-import FAQ from "@/components/sections/FAQ";
-import NewsSection from "@/components/sections/NewsSection";
+import  Header from "../components/layout/Header";
+import  Footer  from "../components/layout/Footer";
+import  Hero  from "../components/sections/Hero";
+import Concerns from "../components/sections/Concerns";
+import ProductInfo from "../components/sections/ProductInfo";
+import  Features  from "../components/sections/Features";
+import Benefits from "../components/sections/Benefits";
+import FAQ from "../components/sections/FAQ";
+import NewsSection from "../components/sections/NewsSection";
 
 const CareSmilyApp: React.FC = () => {
+
   return (
     <div className="flex overflow-hidden flex-col pb-12 bg-white">
       <Header />
@@ -22,7 +23,7 @@ const CareSmilyApp: React.FC = () => {
       <NewsSection />
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default CareSmilyApp;
+export default CareSmilyApp
